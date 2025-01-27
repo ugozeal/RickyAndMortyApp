e7bba9b4ebd3826cbaab8a2924c5c8168cfda185
David Okonkwo <davido@ikhokha.com>
2025-01-27 17:12:07 +0100

    create swift project

---
+++ RickyAndMortyApp/RickyAndMortyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickyAndMortyApp
+//
+//  Created by David Okonkwo on 27/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickyAndMortyApp/RickyAndMortyApp/RickyAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortyAppApp.swift
+//  RickyAndMortyApp
+//
+//  Created by David Okonkwo on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
